77301bd1783a4629371da7ac525be098274ce52d
Michele Condo <mcondokmb@gmail.com>
2025-01-09 11:24:45 +0000

    Added new license file. Updated license across projects files.

--- Eos/MemoryTagPolicy.h
+++ Eos/MemoryTagPolicy.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2018-2025 Michele Condo'
+// File: C:\Projects\Eos\Eos\MemoryTagPolicy.h
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
 #pragma once
 
 #include "Core/BasicTypes.h"
